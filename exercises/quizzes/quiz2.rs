25c3548b05047ffd8f9a6716197d3285e1e4205b
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-09-23 14:20:34 +0800

    Solve quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,15 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input.iter().map(|x| {
+            match x.1 {
+                Command::Uppercase => x.0.to_uppercase(),
+                Command::Trim => x.0.trim().to_string(),
+                Command::Append(t) => x.0.clone() + &"bar".repeat(t),
+            }
+        }).collect()
+    }
 }
 
 fn main() {
@@ -39,6 +48,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
