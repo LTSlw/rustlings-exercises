b51b4f866c608f98ba14ad42a02dc98c823615bd
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-09-27 18:48:27 +0800

    Solve 23-from_into

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,23 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let parsed: Vec<&str> = s.split(",").collect();
+        if parsed.len() != 2 {
+            return Person::default();
+        }
+        for &s in &parsed {
+            if s.is_empty() {
+                return Person::default();
+            }
+        }
+        let name = parsed[0].to_string();
+        let age: Result<u8, _> = parsed[1].parse();
+        match age {
+            Err(_) => Person::default(),
+            Ok(age) => Person { name, age }
+        }
+    }
 }
 
 fn main() {
