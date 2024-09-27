3c2a5794334b0d2bee077874d443f6b04a792c88
LTSlw <38352100+LTSlw@users.noreply.github.com>
2024-09-27 19:07:13 +0800

    Solve 23-from_str

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,18 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parsed = s.split(",");
+        let (Some(name), Some(age), None) = (parsed.next(), parsed.next(), parsed.next()) else {
+            return Err(ParsePersonError::BadLen);
+        };
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        let name = name.to_string();
+        let age: u8 = age.parse().map_err(ParsePersonError::ParseInt)?;
+        Ok(Person { name, age })
+    }
 }
 
 fn main() {
